0ca6dfee7fbb5cae2bb0d7df4fb4331f3bf107ad
Jan Reiners <info@janreiners.de>
2024-04-17 23:36:49 +0200

    fixes

--- stage/sandbox/1-azure/azure-3-key-vault.tf
+++ stage/sandbox/1-azure/azure-3-key-vault.tf
@@ -2,6 +2,10 @@ data "azurerm_client_config" "current" {}
 
 data "azurerm_resource_group" "aks1" {
   name = var.resource_group_name
+
+  depends_on = [
+    azurerm_kubernetes_cluster.aks1
+  ]
 }
 
 resource "azurerm_key_vault" "aks1" {
