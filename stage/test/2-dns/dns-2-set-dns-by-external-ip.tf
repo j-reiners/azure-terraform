718010cdf406dc8f03231c02605467819f5ce457
Jan Reiners <info@janreiners.de>
2024-04-19 09:57:32 +0200

    wildcard entfernt

--- stage/test/2-dns/dns-2-set-dns-by-external-ip.tf
+++ stage/test/2-dns/dns-2-set-dns-by-external-ip.tf
@@ -2,18 +2,6 @@ data "cloudflare_zone" "jr_k8s_de" {
   name = "jr-k8s.de"
 }
 
-resource "cloudflare_record" "k8s_wildcard" {
-  zone_id = data.cloudflare_zone.jr_k8s_de.id
-  name    = "*"
-  value   = data.kubernetes_service.nginx.status.0.load_balancer.0.ingress.0.ip
-  type    = "A"
-  ttl     = 180
-
-  depends_on = [ 
-    data.kubernetes_service.nginx
-  ]
-}
-
 resource "cloudflare_record" "k8s_test_wildcard" {
   zone_id = data.cloudflare_zone.jr_k8s_de.id
   name    = "*.test"
