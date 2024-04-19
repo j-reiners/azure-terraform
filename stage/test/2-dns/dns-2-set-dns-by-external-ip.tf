9014a0c4d40c97d7c98ba26a268ebe8330c6485a
Jan Reiners <info@janreiners.de>
2024-04-19 09:46:17 +0200

    Ordner Pfad geändert

--- stage/test/2-dns/dns-2-set-dns-by-external-ip.tf
+++ stage/test/2-dns/dns-2-set-dns-by-external-ip.tf
@@ -13,3 +13,15 @@ resource "cloudflare_record" "k8s_wildcard" {
     data.kubernetes_service.nginx
   ]
 }
+
+resource "cloudflare_record" "k8s_test_wildcard" {
+  zone_id = data.cloudflare_zone.jr_k8s_de.id
+  name    = "*.test"
+  value   = data.kubernetes_service.nginx.status.0.load_balancer.0.ingress.0.ip
+  type    = "A"
+  ttl     = 180
+
+  depends_on = [ 
+    data.kubernetes_service.nginx
+  ]
+}
